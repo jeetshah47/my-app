31dc123cbdbd81d173247f11255f26fee8a9a1b5
unknown <jeetshahajwa@gmail.com>
2024-06-09 14:30:14 +0530

    ne wrepo

--- components/buttons/Location.tsx
+++ components/buttons/Location.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Button, View, StyleSheet, Text } from 'react-native';
-import * as TaskManager from 'expo-task-manager';
-import * as Location from 'expo-location';
+import React, { useEffect } from "react";
+import { Button, View, StyleSheet, Text } from "react-native";
+import * as TaskManager from "expo-task-manager";
+import * as Location from "expo-location";
 
-const LOCATION_TASK_NAME = 'background-location-task';
+const LOCATION_TASK_NAME = "background-location-task";
 const requestPermissions = async () => {
   const { status: foregroundStatus } =
     await Location.requestForegroundPermissionsAsync();
-  if (foregroundStatus === 'granted') {
+  if (foregroundStatus === "granted") {
     const { status: backgroundStatus } =
       await Location.requestBackgroundPermissionsAsync();
-    if (backgroundStatus === 'granted') {
+    if (backgroundStatus === "granted") {
+      const location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+
       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
         accuracy: Location.Accuracy.Balanced,
       });
@@ -18,22 +21,27 @@ const requestPermissions = async () => {
   }
 };
 
-const PermissionsButton = () => (
-  <View style={styles.container}>
-    <Button onPress={requestPermissions} title="Enable background location" />
-  </View>
-);
+const PermissionsButton = () => {
+    useEffect(() => {
+            requestPermissions();
+    },[])
+  return (
+    <View style={styles.container}>
+      <Button onPress={requestPermissions} title="Enable background location" />
+    </View>
+  );
+};
 
 TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
   if (error) {
     console.log(error);
-    
+
     // Error occurred - check `error.message` for more details.
     return;
   }
   if (data) {
     // const { locations } = data;
-    console.log(":pa",data);
+    console.log(":pa", data);
     // locate = locations;
     // do something with the locations captured in the background
   }
@@ -42,8 +50,8 @@ TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
